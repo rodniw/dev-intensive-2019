d80fb22bb1a2ffdbb91a1042ebcc5227f76c29ea
rodion <rodion.prosvirnin@bk.ru>
2019-07-02 03:24:49 +0300

    complete hometask_2

--- app/src/main/java/ru/skillbranch/devintensive/myapplication/models/Chat.kt
+++
@@ -1,8 +0,0 @@
-package ru.skillbranch.devintensive.myapplication.models
-
-class Chat(
-    val id: String,
-    val members: MutableList<User> = mutableListOf(),
-    val messages: MutableList<BaseMessage> = mutableListOf()
-) {
-}
